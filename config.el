b31f142c7864cffc6af44d98dfa137b07546c2d9
ParetoOptimalDev <pareto.optimal@mailfence.com>
2023-03-14 18:53:28 -0500

    use haskell docker image that customizes PATH

--- config.el
+++ config.el
@@ -1,17 +1,22 @@
+(require 'tramp)
 (message "")
-(message (concat "correct path: " "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"))
 
-(message "show there is no bug if tramp-own-remote-path is at front of tramp-remote-path")
+(message "using the docker haskell image")
+(message (concat "correct path is: " "/root/.cabal/bin:/root/.local/bin:/opt/ghc/9.4.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"))
 
-(let ((default-directory (concat "/docker:" (getenv "container_id") ":"))
-      (tramp-remote-path '(tramp-own-remote-path)))
-  (message "")
-  (message (concat "tramp-remote-path: " (format "%S" tramp-remote-path)))
-  (message (concat "default-directory: " default-directory))
-  (message (concat "path is: " (truncate-string-to-width (getenv "PATH") 100)))
-  (message ""))
+(message "show what happens with tramp-own-remote-path at front")
+(let ((docker-container-id (string-trim (shell-command-to-string "docker run -it --rm --detach haskell"))))
+  (let ((default-directory (concat "/docker:" docker-container-id ":"))
+	(tramp-remote-path '(tramp-own-remote-path)))
+        (message docker-container-id)
+	(message (concat "path is: " (truncate-string-to-width (eshell-command-result "echo $PATH") 100)))))
+
+(message "simluate what nix does now and demonstrate bug")
+
+(let ((docker-container-id (string-trim (shell-command-to-string "docker run -it --rm --detach haskell"))))
+  (let ((default-directory (concat "/docker:" docker-container-id ":"))
+	(tramp-remote-path '("/nix/something" tramp-own-remote-path)))
+        (message docker-container-id)
+	(message (concat "path is: " (truncate-string-to-width (eshell-command-result "echo $PATH") 100)))))
 
 
-(message "TODO simluate what nix does now and demonstrate bug")
-
-;; (let ((default-directory "/docker:8bd61beae4f7ff829396a548fe1a5067d62f95597a2c18a1fdd5f93c5e8d111a:")) (getenv "PATH"))
