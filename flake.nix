ec7427d50e15f01eea6e469d8802278380110b02
ParetoOptimalDev <pareto.optimal@mailfence.com>
2023-03-14 18:22:42 -0500

    remove non devshell stuff

--- flake.nix
+++ flake.nix
@@ -5,11 +5,6 @@
     let pkgs = import nixpkgs { system = "x86_64-linux";};
     in
       {
-
-        packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-        packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
         devShell.x86_64-linux = pkgs.mkShell {
           buildInputs = with pkgs; [ emacs docker ];
           shellHook = ''
